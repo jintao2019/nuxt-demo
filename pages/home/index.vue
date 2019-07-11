34bec49bf29aedaf8283363977393b092f073f8f
vcode <15019229979@163.com>
2019-07-11 18:11:40 +0800

    添加插件模块

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="container">
     <div>这是home</div>
+		<notifications group="foo" />
   </div>
 </template>
 
 <script>
 export default {
-    
+	mounted() {
+		this.$notify({
+			group: 'foo',
+			title: 'Important message',
+			text: 'Hello user! This is a notification!'
+		});
+	}
 }
 </script>
 
